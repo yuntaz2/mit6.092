31cf00cf037f4cd75cff34750ffda75a210fd45a
yuntaz2 <yuntaz2@uci.edu>
2022-06-26 13:19:35 -0700

    Finish assign6 I think

--- lecture6/DrawGraphics.java
+++ lecture6/DrawGraphics.java
@@ -1,18 +1,26 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class DrawGraphics {
-    Bouncer movingSprite;
-
+    private static int index = 0;
+    private static ArrayList<Bouncer> bouncerObjs = new ArrayList<Bouncer>();
     /** Initializes this class for drawing. */
     public DrawGraphics() {
         Rectangle box = new Rectangle(15, 20, Color.RED);
-        movingSprite = new Bouncer(100, 170, box);
-        movingSprite.setMovementVector(3, 1);
+        bouncerObjs.add(new Bouncer(100, 170, box));
+        bouncerObjs.get(index).setMovementVector(3, 1);
+        index++;
+        Oval circle = new Oval(22, 33, Color.green);
+        bouncerObjs.add(new Bouncer(100, 170, circle));
+        bouncerObjs.get(index).setMovementVector(-3, -1);
+        index++;
     }
 
     /** Draw the contents of the window on surface. */
-    public void draw(Graphics surface) {
-        movingSprite.draw(surface);
+    public static void draw(Graphics surface) {
+        for (int i = 0; i < index; ++i) {
+            bouncerObjs.get(i).draw(surface);
+        }
     }
 }
